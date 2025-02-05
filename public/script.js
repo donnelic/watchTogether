5bdd199a0d513ac23718a9646dcebf5ae7301760
Doron Licht <doronlicht@gmail.com>
2025-02-05 17:25:10 +0100

    version 1.i am stupid

--- public/script.js
+++ public/script.js
@@ -1,56 +1,68 @@
-const socket = io("https://watch-together-iota.vercel.app/api/socket");
-        const video = document.getElementById('video');
-        const urlInput = document.getElementById('m3u8Url');
-        const title = document.getElementById('title');
+const socket = io('https://watch-together-iota.vercel.app/api/socket')
 
-        socket.on("updateVideo", (url) => {
-            if (video.src !== url) {
-                loadStream(url);
-            }
-        });
+socket.on('connect', () => {
+  console.log('Socket connected')
+})
 
-        socket.on("sync", ({ time, playing }) => {
-            if (Math.abs(video.currentTime - time) > 0.5) {
-                video.currentTime = time;
-            }
-            if (playing) {
-                video.play();
-            } else {
-                video.pause();
-            }
-        });
+// Add other socket event listeners as needed
 
-        function setStream() {
-            const url = urlInput.value;
-            socket.emit("setVideo", url);
-            loadStream(url);
-            video.style.display = "block";
-            title.style.display = "none";            
+const video = document.getElementById('video')
+const urlInput = document.getElementById('m3u8Url')
+const title = document.getElementById('title')
+
+socket.on('updateVideo', url => {
+  if (video.src !== url) {
+    loadStream(url)
+  }
+})
+
+socket.on('sync', ({ time, playing }) => {
+  if (Math.abs(video.currentTime - time) > 0.5) {
+    video.currentTime = time
+  }
+  if (playing) {
+    video.play()
+  } else {
+    video.pause()
+  }
+})
+
+function setStream () {
+  const url = urlInput.value
+  socket.emit('setVideo', url)
+  loadStream(url)
+  video.style.display = 'block'
+  title.style.display = 'none'
+}
+
+function loadStream (url) {
+  if (Hls.isSupported()) {
+    const hls = new Hls()
+    hls.loadSource(url)
+    hls.attachMedia(video)
+    hls.on(Hls.Events.ERROR, function (event, data) {
+      if (data.fatal) {
+        switch (data.fatal) {
+          case Hls.ErrorTypes.NETWORK_ERROR:
+            alert('Network error encountered while loading the video.')
+            break
+          case Hls.ErrorTypes.OTHER_ERROR:
+            alert('An error occurred while loading the video.')
+            break
         }
+      }
+    })
+  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = url
+  }
+}
 
-        function loadStream(url) {
-            if (Hls.isSupported()) {
-                const hls = new Hls();
-                hls.loadSource(url);
-                hls.attachMedia(video);
-                hls.on(Hls.Events.ERROR, function (event, data) {
-                    if (data.fatal) {
-                        switch (data.fatal) {
-                            case Hls.ErrorTypes.NETWORK_ERROR:
-                                alert('Network error encountered while loading the video.');
-                                break;
-                            case Hls.ErrorTypes.OTHER_ERROR:
-                                alert('An error occurred while loading the video.');
-                                break;
-                        }
-                    }
-                });
-            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                video.src = url;
-            }
-        }
-        
-
-        video.addEventListener("play", () => socket.emit("sync", { time: video.currentTime, playing: true }));
-        video.addEventListener("pause", () => socket.emit("sync", { time: video.currentTime, playing: false }));
-        video.addEventListener("seeked", () => socket.emit("sync", { time: video.currentTime, playing: !video.paused }));
+video.addEventListener('play', () =>
+  socket.emit('sync', { time: video.currentTime, playing: true })
+)
+video.addEventListener('pause', () =>
+  socket.emit('sync', { time: video.currentTime, playing: false })
+)
+video.addEventListener('seeked', () =>
+  socket.emit('sync', { time: video.currentTime, playing: !video.paused })
+)
