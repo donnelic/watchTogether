2e32fedd49f1c6273c5c784268c8c1840851fc3d
Doron Licht <doronlicht@gmail.com>
2025-02-05 17:17:16 +0100

    version 1.i am stupid

--- api/socket.js
+++ api/socket.js
@@ -1,34 +1,37 @@
 const { Server } = require("socket.io");
 
-const io = new Server({
-    cors: {
-        origin: "*"
-    },
-});
-
-let currentVideo = "";
-let currentState = { time: 0, playing: false };
-
-io.on("connection", (socket) => {
-    socket.emit("updateVideo", currentVideo);
-    socket.emit("sync", currentState);
-
-    socket.on("setVideo", (url) => {
-        currentVideo = url;
-        io.emit("updateVideo", url);
-    });
-
-    socket.on("sync", (state) => {
-        currentState = state;
-        socket.broadcast.emit("sync", state);
-    });
-});
+// Initialize a variable to hold the socket server
+let io;
 
 // Export as a serverless function
-module.exports = (req, res) => {
-    if (req.method === "GET") {
-        io(req, res);
-    } else {
-        res.status(405).end(); // Method Not Allowed
+export default function handler(req, res) {
+    // Initialize socket.io only once
+    if (!io) {
+        io = new Server(res.socket.server, {
+            cors: {
+                origin: "*",
+            },
+        });
+
+        io.on("connection", (socket) => {
+            console.log("New client connected");
+
+            // Emit the current video and state to the newly connected client
+            socket.emit("updateVideo", currentVideo);
+            socket.emit("sync", currentState);
+
+            socket.on("setVideo", (url) => {
+                currentVideo = url;
+                io.emit("updateVideo", url);
+            });
+
+            socket.on("sync", (state) => {
+                currentState = state;
+                socket.broadcast.emit("sync", state);
+            });
+        });
     }
-};
+
+    // Respond to the initial request
+    res.end();
+}
